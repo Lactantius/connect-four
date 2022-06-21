3161cb32740a547bf57beb4420529f92622da582
Gerard Keiser <gerardkeiser@gmail.com>
2022-06-21 11:18:47 -0500

    Tests passing for handleClick

--- connect4.js
+++ connect4.js
@@ -8,8 +8,10 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
+let board = makeBoard();
+
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+// const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -22,25 +24,25 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.querySelector("#board");
 
-  // TODO: add comment for this code
+  // Make top row of the game board
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
+  // Make cells for the top row
+  for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  // Make other rows
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -52,8 +54,11 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = 0; y < HEIGHT; y++) {
+    console.log(y);
+    if (!board[y][x]) return y;
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -72,17 +77,17 @@ function endGame(msg) {
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
+  board[y][x] = currPlayer;
 
   // check for win
   if (checkForWin()) {
@@ -93,7 +98,7 @@ function handleClick(evt) {
   // TODO: check if all cells in board are filled; if so call, call endGame
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? (currPlayer = 2) : (currPlayer = 1);
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -150,5 +155,4 @@ function checkForWin() {
   }
 }
 
-makeBoard();
 makeHtmlBoard();
--- connect4.test.js
+++ connect4.test.js
@@ -1,6 +1,6 @@
 describe("Board is generated", function () {
   it("should make an arry of arrays, with all elements null", function () {
-    const board = [
+    const testBoard = [
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
@@ -8,6 +8,48 @@ describe("Board is generated", function () {
       [null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null],
     ];
-    expect(makeBoard()).toEqual(board);
+    expect(makeBoard()).toEqual(testBoard);
+  });
+});
+
+describe("handleClick(evt) updates board properly", function () {
+  beforeEach(function () {
+    board = makeBoard();
+    currPlayer = 1;
+  });
+  it("should replace null values with player numbers", function () {
+    const updatedBoard = [
+      [1, 2, null, null, null, null, null],
+      [1, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+    ];
+
+    document.getElementById(0).click();
+    document.getElementById(1).click();
+    document.getElementById(0).click();
+    expect(board).toEqual(updatedBoard);
+  });
+
+  it("should reject clicks that won't fit on the board", function () {
+    const fullColBoard = [
+      [1, null, null, null, null, null, null],
+      [2, null, null, null, null, null, null],
+      [1, null, null, null, null, null, null],
+      [2, null, null, null, null, null, null],
+      [1, null, null, null, null, null, null],
+      [2, null, null, null, null, null, null],
+    ];
+
+    for (let y = 0; y < HEIGHT; y++) {
+      document.getElementById(0).click();
+    }
+
+    const player = currPlayer;
+    document.getElementById(0).click();
+    expect(currPlayer).toEqual(player);
+    expect(board).toEqual(fullColBoard);
   });
 });
